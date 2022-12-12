e66758f8b4344bcb0b92a15d83fb81097632f445
Samuel Almeida <75258537+SamuelAlmeidaS@users.noreply.github.com>
2022-12-12 10:17:38 -0300

    test database

---
+++ database.js
@@ -0,0 +1,8 @@
+const randomImg = [
+    {img:'./assets/randomImg/random 1.jpg'},
+    {img:'./assets/randomImg/random 1.jpg'},
+    {img:'./assets/randomImg/random 1.jpg'},
+    {img:'./assets/randomImg/random 1.jpg'},
+    {img:'./assets/randomImg/random 1.jpg'},
+    {img:'./assets/randomImg/random 1.jpg'}
+]
