1fa5185d9b4972614a578ba91cbf8aab95b07342
Samuel Almeida <75258537+SamuelAlmeidaS@users.noreply.github.com>
2022-12-14 14:56:42 -0300

    80% completed

--- database.js
+++
@@ -1,8 +0,0 @@
-const randomImg = [
-    {img:'./assets/randomImg/random 1.jpg'},
-    {img:'./assets/randomImg/random 1.jpg'},
-    {img:'./assets/randomImg/random 1.jpg'},
-    {img:'./assets/randomImg/random 1.jpg'},
-    {img:'./assets/randomImg/random 1.jpg'},
-    {img:'./assets/randomImg/random 1.jpg'}
-]
--- main.js
+++
@@ -1,6 +0,0 @@
-const imgProject = document.querySelectorAll('.img-project');
-randomImg.map(item => {
-    imgProject.src = item.img;
-});
-
-console.log(imgProject)
